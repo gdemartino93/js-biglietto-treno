1b797b3856d9850ce0b346ad3c22e0d4a69d5456
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-07 16:31:07 +0100

    tofixed

--- main.js
+++ main.js
@@ -2,24 +2,22 @@
 let age=parseInt(prompt("Inserisci la tua età"));
 let numeroKm=parseInt(prompt("Inserisci il numero di km che vuoi percorrere"))
 // prezzo fisso del biglietto
-const prezzo=2;
-const prezzointero=(numeroKm * prezzo);
+const prezzo=0.21;
+const prezzointero=(numeroKm * prezzo).toFixed(2);
 
 // sconto minorenni
 if (age <= 17) {
     let sconto= (numeroKm * prezzo) * 20 / 100;
-    let costo=(prezzointero - sconto);
+    let costo=(prezzointero - sconto).toFixed(2);
     document.getElementById("testo").innerHTML=`Vuoi percorrere ${numeroKm}km ed hai ${age}anni quindi il prezzo scontato del 20% è di ${costo} euro`;
 }
 // sconto età over 65 
 else if ( age >= 65){
     let sconto= (numeroKm * prezzo) * 40 / 100;
-    let costo=(prezzointero - sconto);
-    console.log(costo.toFixed(2));
+    let costo=(prezzointero - sconto).toFixed(2);
     document.getElementById("testo").innerHTML=`Vuoi percorrere ${numeroKm}km ed hai ${age} anni quindi il prezzo scontato deò 40% è di ${costo} euro`;
 }
 // età compresa
     else{
         document.getElementById("testo").innerHTML=`Vuoi percorrere ${numeroKm}km ed hai ${age} anni quindi non usufruendo di nessuno sconto il prezzo del biglietto è di ${prezzointero} euro`
-        console.log(prezzointero.toFixed(2));
     }
